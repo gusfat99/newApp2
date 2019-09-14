b5ffd2c5a0fdd8a94e5089ad66e9798f4a821ca8
Bagus Fatwan Alfiat <fatwanbagus.99@gmail.com>
2019-09-15 03:16:53 +0700

    logic handler textInput

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, TextInput, Text, View } from 'react-native';
 import styles from "./assets/styles";
 
 export default function App() {
+  const  [enteredGoals, setEnteredGoals] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalsInputHandler = (enterdText) => {
+    setEnteredGoals(enterdText);
+  }
+  const addGoalsHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoals]);
+  }
+
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Goals" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput placeholder="Goals" style={styles.input} onChangeText={goalsInputHandler} />
+        <Button title="ADD" onPress={addGoalsHandler} />
       </View>
       <View>
-       
+        {courseGoals.map((courseGoal) => 
+            <View>
+              <Text>{courseGoal}</Text>
+            </View>
+          )}
       </View>
     </View>
   );
