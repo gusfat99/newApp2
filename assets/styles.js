6449f658eeab4457155b57796b00886c1168e2f4
Bagus Fatwan Alfiat <fatwanbagus.99@gmail.com>
2019-09-15 02:53:25 +0700

    menambahkan file style.js pada assets

---
+++ assets/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+    container : {
+        flex: 1,
+        marginHorizontal: 12,
+    },
+    inputContainer : {
+        flexDirection: 'row',
+        justifyContent:'space-between',
+    }
+});
